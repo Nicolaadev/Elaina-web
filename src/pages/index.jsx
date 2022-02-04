710effb1377a21bda44dc86b9b0e9095e6e81632
Nicol <74636982+NikoP100@users.noreply.github.com>
2022-02-04 17:02:07 +0800

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -29,13 +29,11 @@ function HomepageHeader() {
               <Link className="button button--secondary button--lg" to="/docs/about" >
               Get Started
               </Link>
-<<<<<<< HEAD
 
               <Link className="button button--secondary button--lg" to="https://elaina.pages.dev">
               Invite Elaina
               </Link>
           </div>      
-=======
   
               <Link className="button button--secondary button--lg" to="https://discord.com/api/oauth2/authorize?client_id=875563372974850059&permissions=3533120&scope=bot%20applications.commands">
               Invite Elaina
@@ -45,59 +43,6 @@ function HomepageHeader() {
               Invite Elaina 2.0
               </Link>
           </div>
-
-
-    <div className="App">
-      <div>
-        <div>
-
-          <div className='title'>
-            <b>Play your favourite song</b>
-          </div>
-
-        </div>
-
-          <div className="item">
-            <div className="text">
-              <div className='des'>
-              Play your favourite song anytime 24/7 with the use of Elaina, our best to become music bot. You can play your favourite songs using Youtube/Spotify links or by searching songs with Elaina.
-              </div>
-            </div>
-
-            <div className="hero1">
-              <img src="img/img1.png" className='img2'/>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="App1">
-      <div>
-        <div>
-
-          <div className='title_alt'>
-            <b>Find the songs lyrics</b>
-          </div>
-
-        </div>
-
-          <div className="item">
-            <div className="text_alt">
-              <div className='des_alt'>
-              It takes two words, artist name and song name and it will show you the lyrics of the song 
-              </div>
-              <div className='des_alt'>
-              with the use of Elaina, our best music bot.
-              </div>
-            </div>
-
-            <div className="hero1_alt">
-              <img src="img/lyrics.png" className='img3'/>
-            </div>
-          </div>
-        </div>
-      </div>
->>>>>>> 6d9675b44ff6959112d3b2eff4425a0bab2fec2d
-    </div>
     </header>
   );
   }
